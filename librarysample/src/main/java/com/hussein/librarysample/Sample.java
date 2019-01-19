474c4c78ba1a9b8f23bc68a9aef476339c7949fe
Hussein <hussein0116>
2019-01-19 23:30:44 +0200

    SampleApp

---
+++ librarysample/src/main/java/com/hussein/librarysample/Sample.java
@@ -0,0 +1,28 @@
+package com.hussein.librarysample;
+
+public class Sample {
+    private String name;
+
+    public Sample(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String phone;
+}
